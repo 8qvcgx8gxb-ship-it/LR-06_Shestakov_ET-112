97c5a87e4bf104100d75ac50c26e76e964ae8cdb
AlexShe <8qvcgx8gxb@privaterelay.appleid.com>
2026-03-16 21:48:19 +0500

    test

--- LR-06_Shestakov_ET-112/main.cpp
+++ LR-06_Shestakov_ET-112/main.cpp
@@ -9,6 +9,7 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    std::cout << "Hello, World!123\n";
+    std::cout << "test";
     return EXIT_SUCCESS;
 }
