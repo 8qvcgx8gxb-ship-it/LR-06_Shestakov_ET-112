e59c67477f3c5ac33e97a63cd3ecd90aaac564ca
AlexShe <8qvcgx8gxb@privaterelay.appleid.com>
2026-03-20 12:45:41 +0500

    пример

--- LR-06_Shestakov_ET-112/main.cpp
+++ LR-06_Shestakov_ET-112/main.cpp
@@ -9,5 +9,15 @@
 using namespace std;
 
 int main() {
-
+    int n = 0;
+    cin >> n;
+    int arr[n];
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    for(int i = 0; i < n; i++){
+        cout << arr[i] << " ";
+    }
+    
+    return 0;
 }
